d5416d4ec70b5b028ac00ca808df48ce60d264ec
Yannick Assogba <yassogba@gmail.com>
2017-05-16 17:31:13 -0400

    inital commit

---
+++ script.js
@@ -0,0 +1,6 @@
+require(["voyager"], function(voyager) {
+	// const node = document.getElementById('embedHere')
+	// const node = document.querySelector('#embedHere')
+	// const voyagerInstance = voyager.CreateVoyager(node, {}, []);
+	const voyagerInstance = voyager.CreateVoyager('#embedHere', {}, []);
+})
